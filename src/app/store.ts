9e535c5f16261b21824e2642abb280d7383d5b58
Joel <joelsetterberg@hotmail.com>
2022-10-03 14:51:49 +0200

    add comment

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 //Import all slices
-import counterReducer from '../features/somefeature/somefeature-slice';
+import somefeatureReducer from '../features/somefeature/somefeature-slice';
 
 //Import all api slices
 import { apiSlice } from '../features/someotherfeature/someotherfeature-api-slice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    //add slices and api slices
+    somefeature: somefeatureReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => {
